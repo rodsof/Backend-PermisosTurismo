a2f85f7c8dc830135e29d65fa624b809e0fc7144
sofia <rodriguezsofiaf@gmail.com>
2021-05-23 19:27:17 -0300

    AGregue permisosturismo.herokuapp.com a ALLOWED_HOSTS

--- parcial/settings.py
+++ parcial/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-ALLOWED_HOSTS = ['https://permisosturismo.herokuapp.com/']
+ALLOWED_HOSTS = ['permisosturismo.herokuapp.com']
 
 
 # Application definition
