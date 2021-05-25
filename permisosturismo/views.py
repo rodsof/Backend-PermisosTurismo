d63c1685047d1eae4f330c757ae210d36625e3cd
sofia <rodriguezsofiaf@gmail.com>
2021-05-25 15:43:10 -0300

    Cambios en views :smile:

--- permisosturismo/views.py
+++ permisosturismo/views.py
@@ -5,20 +5,24 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CiudadanoViewSet(viewsets.ModelViewSet):
+   "A continuación se encuentra la vista para crear, modificar, obtener o borrar los ciudadanos registrados en el sistema"
    queryset = Ciudadano.objects.all()
    serializer_class = CiudadanoSerializer
    filter_backends = [DjangoFilterBackend]
    filterset_fields = ['nroDoc']
  
 class EventoViewSet(viewsets.ModelViewSet):
+   "A continuación se encuentra la vista para crear, modificar, obtener o borrar los eventos registrados en el sistema"
    queryset = Evento.objects.all()
    serializer_class = EventoSerializer
 
 class DomicilioViewSet(viewsets.ModelViewSet):
+   "A continuación se encuentra la vista para crear, modificar, obtener o borrar los domicilios registrados en el sistema"
    queryset = Domicilio.objects.all()
    serializer_class = DomicilioSerializer
  
 class PermisoViewSet(viewsets.ModelViewSet):
+   "A continuación se encuentra la vista para crear, modificar, obtener o borrar los permisos registrados en el sistema"
    queryset = Permiso.objects.all()
    serializer_class = PermisoSerializer
 
